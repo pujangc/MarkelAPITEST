1b2eae8ebe3cd3c3842f89cf4a27f83ceb2ec771
Pujan_gc <pujan.choudhury@bybox.com>
2023-02-21 21:20:01 +0000

    initial api commit

---
+++ Markel_API/Models/Claims.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Text.Json.Serialization;
+
+namespace Markel_API.Models
+{
+    public class Claims
+    {
+        public string UCR { get; set; } = string.Empty;
+        [Required]
+        public int CompanyId { get; set; }
+        public DateTime ClaimDate { get; set; }
+        public DateTime LossDate { get; set; }
+        public string AssuredName { get; set; } = string.Empty;
+        public decimal IncurredLoss { get; set; }
+        public bool Closed { get; set; }
+        public double AgeOfTheClaim { get; set; }
+
+        [JsonIgnore]
+        public virtual Company? Company { get; set; }
+    }
+}
---
+++ Markel_API/Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Metrics;
+using System;
+
+namespace Markel_API.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address1 { get; set; } = string.Empty;
+        public string Address2 { get; set; } = string.Empty;
+        public string Address3 { get; set; } = string.Empty;
+        public string Postcode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public bool Active { get; set; }
+        public bool HasActiveInsurancePolicy { get; set; }
+        public DateTime InsuranceEndDate { get; set; }
+        public virtual List<Claims> Claims { get; set; }
+    }
+}
